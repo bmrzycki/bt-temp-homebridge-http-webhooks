d33690b38b374e1393caf4dc607087413b5f41a6
Brian Rzycki <brzycki@gmail.com>
2021-11-16 18:19:46 -0600

    btscan: add more information
    Added rssi min, rssi max, rssi avg, and battery %.

--- btscan.py
+++ btscan.py
@@ -6,25 +6,33 @@ from signal import signal, SIGPIPE, SIG_DFL
 from threading import Lock
 from time import sleep
 
-_BT = set()
+_BT = {}
 _LOCK = Lock()
-_NAME_MAXLEN = 0
+_MAXLEN_NAME = 0
+
+def _add(mac, model, ad):
+    global _MAXLEN_NAME
+    try:
+        _LOCK.acquire()
+        d = _BT.get(mac, { 'model' : model,
+                           'batt'  : ad.mfg_data[-2],
+                           'rssi'  : [] })
+        d['name'] = ad.name  # save most recent name, may change
+        d['rssi'].append(ad.rssi)
+        _BT[mac] = d
+        _MAXLEN_NAME = max(_MAXLEN_NAME, len(ad.name))
+    finally:
+        _LOCK.release()
+
 
 def callback(ad):
-    global _NAME_MAXLEN
-    if ad.mfg_data:
-        mac = ad.address.address
-        try:
-            _LOCK.acquire()
-            if (mac.startswith('A4:C1:38:') and
-                bleson.UUID16(0xec88) in ad.uuid16s):
-                _BT.add((mac, 'Govee H5075', ad.name))
-                _NAME_MAXLEN = max(_NAME_MAXLEN, len(ad.name))
-            elif mac.startswith('49:42:08:') and len(ad.mfg_data) == 9:
-                _BT.add((mac, 'Inkbird IBS-TH2', ad.name))
-                _NAME_MAXLEN = max(_NAME_MAXLEN, len(ad.name))
-        finally:
-            _LOCK.release()
+    if not ad.mfg_data:
+        return
+    mac = ad.address.address
+    if mac.startswith('A4:C1:38:') and bleson.UUID16(0xec88) in ad.uuid16s:
+        _add(mac, 'Govee H5075    ', ad)
+    elif mac.startswith('49:42:08:') and len(ad.mfg_data) == 9:
+        _add(mac, 'Inkbird IBS-TH2', ad)
 
 
 def main():
@@ -41,11 +49,17 @@ def main():
     finally:
         o.stop()
 
-    if _BT:
-        print(f"Found {len(_BT)} potential devices:")
-        for m, k, n in sorted(list(_BT)):
-            n = n.ljust(_NAME_MAXLEN)
-            print(f"  {m}  {n}  {k}")
+    if not _BT:
+        print("No supported devices found")
+        return
+
+    print(f"Found {len(_BT)} supported device(s):")
+    for k in sorted(_BT):
+        d = _BT[k]
+        r, n = sorted(d['rssi']), d['name'].ljust(_MAXLEN_NAME)
+        rm, rx, ra = r[0], r[-1], sum(r)/float(len(r))
+        # mac  model  name  rssi_min  rssi_max rssi_avg  battery_%
+        print(f"  {k}  {d['model']}  {n}  {rm}  {rx}  {ra:.2f}  {d['batt']}%")
 
 
 if __name__ == '__main__':
